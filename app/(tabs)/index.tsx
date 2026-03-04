fb6693824e8116c5475efd2a7353f7095844bd2b
haziq769 <haziq769ahmad@gmail.com>
2026-03-04 14:45:24 +0800

    add style, add header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,74 @@
-import { api } from "@/convex/_generated/api";
-import { useMutation, useQuery } from "convex/react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createHomeStyles } from "@/assets/styles/home.styles";
+import Header from "@/components/Header";
+import { LinearGradient } from "expo-linear-gradient";
+import { StatusBar, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import useTheme from "../hooks/useTheme";
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-  const todos = useQuery(api.todos.getTodos);
-  console.log(todos);
-  const addTodo = useMutation(api.todos.addTodo);
+  // const styles = createStyles(colors);
+
+  const homeStyles = createHomeStyles(colors);
+
+  // const todos = useQuery(api.todos.getTodos);
+  // console.log(todos);
+  // const addTodo = useMutation(api.todos.addTodo);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>
+    <LinearGradient
+      colors={colors.gradients.background}
+      style={homeStyles.container}
+    >
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Header />
+        {/* <Text style={styles.content}>
         Edit app/index.tsx to edit this screen.
-      </Text>
+      </Text> */}
 
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text>toggle the mode</Text>
-      </TouchableOpacity>
+        <TouchableOpacity onPress={toggleDarkMode}>
+          <Text>toggle the mode</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
+        {/* <TouchableOpacity onPress={() => addTodo({ text: "walk the dog" })}>
         <Text>Add a new to do</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
 
-      {/* <Link href={"/about"}>About screen</Link> */}
-    </View>
+        {/* <Link href={"/about"}>About screen</Link> */}
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, //is column direction by default
-    // flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "red",
-  },
-  content: {
-    fontSize: 52,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, //is column direction by default
+//     // flexDirection: "row",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     backgroundColor: "red",
+//   },
+//   content: {
+//     fontSize: 52,
+//   },
+// });
+
+// const createStyles = (colors:ColorScheme) => {
+//   const styles = StyleSheet.create({
+//   container: {
+//     flex: 1, //is column direction by default
+//     // flexDirection: "row",
+//     justifyContent: "center",
+//     alignItems: "center",
+//     gap:10,
+//     backgroundColor:colors.bg,
+
+//   },
+//   content: {
+//     fontSize: 52,
+//   },
+// });
+// return styles;
+// }
